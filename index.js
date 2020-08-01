42ac0203a19ba0bac61960dcee54fbb758ddc8cf
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2020-08-02 00:12:11 +0100

    Trial task data replacement within Epic.

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ async function run() {
      * identify the issue as an actual Epic.
      */
     if (sourceIssue.title.startsWith(epicPrefix))
-        updateEpic(octokit, context, sourceIssue);
+        updateEpicIssue(octokit, context, sourceIssue);
     else
-        updateTask(octokit, context, sourceIssue);
+        updateEpicFromTask(octokit, context, sourceIssue);
 }
 
 // Update Epic issue
-async function updateEpic(octokit, context, issue) {
+async function updateEpicIssue(octokit, context, issue) {
     console.log("Updating Epic issue '" + issue.title + "'...");
     console.log("  -- Issue number is " + issue.number);
     console.log("  -- Issue body is '" + issue.body + "'");
@@ -74,8 +74,8 @@ async function updateEpic(octokit, context, issue) {
     }
 }
 
-// Update Task issue
-async function updateTask(octokit, context, taskIssue) {
+// Update Task issue within Epic
+async function updateEpicFromTask(octokit, context, taskIssue) {
     console.log("Updating task issue '" + taskIssue.title + "'...");
     console.log("  -- Issue number is " + taskIssue.number);
     console.log("  -- Issue body is '" + taskIssue.body + "'");
@@ -93,16 +93,86 @@ async function updateTask(octokit, context, taskIssue) {
     console.log(timeline);
     // Look for 'cross-referenced' events, and check if those relate to Epics
     for (event of timeline.data) {
-        if (event.event != "cross-referenced") continue;
+        if (event.event != "cross-referenced")
+            continue;
         console.log("FOUND A CROSS-REFERENCED TIMELINE EVENT:");
         console.log(event.source);
 
-        // Get the referencing issue number from the 'source' data
+        // If the cross-referencing event is not an issue, continue
+        if (event.source.type != "issue")
+            continue;
+
+        // Get referencing issue
+        const refIssue = event.source.issue;
+
+        // Is the cross-referencing issue an Epic?
+        if (!refIssue.title.startsWith(epicPrefix))
+            continue;
+
+        // Update the Epic issue body based on our own data if necessary
+        var newBody = updateTaskInEpic(refIssue.body, taskIssue);
+        console.log("UPDATED EPIC BODY:");
+        console.log(newBody);
+        // Get the referencing issue number / from the 'source' data
 //         const refIssue = await octokit.issues.get({
 //             ...context.repo,
 //             issue_number: event.source.number
 //         });
     }
+}
+
+// Update task within supplied body text from issue data given
+async function updateTaskInEpic(epicBody, taskIssue) {
+    var inWorkload = false
+    var body = epicBody.split(/\r?\n/g);
+    var newBody = "";
+    var nBodyLines = body.length;
+    for (var i = 0; i < nBodyLines; ++i) {
+        // Check for heading, potentially indicating the start of the workload section
+        if (body[i].startsWith("#")) {
+            if (body[i].endsWith("Workload")) {
+                inWorkload = true;
+                continue;
+            }
+            else if (inWorkload)
+                break;
+        }
+
+        // If we are not in the workload section, no need to do anything else
+        if (!inWorkload)
+            continue;
+
+        // Does the line start with checkbox MD, indicating a task?
+        console.log("TEST LINE: " + body[i]);
+        const matchExpression = /(?<pre> *)- \[(?<closed>x| )\] #(?<number>[0-9+]) *(?<title>.*)/g;
+        var match = matchExpression.exec(body[i]);
+        if (!match)
+            continue;
+        console.log(match.groups);
+
+        // Does the taskIssue number match the one on this line?
+        if (match.groups.number != taskIssue.number)
+            continue;
+
+        // Found the taskIssue in the list, so check its status and update as necessary
+        var update = false;
+        const taskIssueClosed = taskIssue.state === "closed" ? "x" : " ";
+        if (match.groups.closed != taskIssueClosed)
+            update = true;
+        if (match.groups.title != taskIssue.title)
+            update = true;
+
+        // Return null if no updates were necessary
+        if (!update)
+            return null;
+
+        // Reconstitute the line, and break out of the loop
+        body[i] = match.groups.pre + "- [" + taskIssueClosed + "] #" + match.groups.number + " " + taskIssue.title + " MARKER";
+        break;
+    }
+
+    // Reconstitute and return updated body text
+    return body.join("\r\n");
 }
 
 // Run the action
